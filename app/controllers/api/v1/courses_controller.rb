088885a85b994b7a51134b2c87f7d24b3b5e7a4b
WerkerM <michael.handwerker@gmail.com>
2016-06-17 14:50:47 -0400

    deserialized new course data

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -2,8 +2,35 @@ module Api
   module V1
     class CoursesController < ApplicationController
       def index
-        render json: Course.includes(:studio, :customers)
+        render json: Course.includes(:studio, :customers), include: ['studio']
       end
+
+      def create
+        new_course = Course.create!(course_params)
+      end
+
+      private
+
+      def course_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+      end
+
+      # private
+      # def course_params
+      #   params
+      #     .require(:data)
+      #     .require(:attributes)
+      #     .permit(:title, :instructor, :description)
+      # end
+      #
+      # def studio_params
+      #   params
+      #     .require(:data)
+      #     .require(:relationships)
+      #     .require(:studio)
+      #     .require(:data)
+      #     .permit(:id)
+      # end
     end
   end
 end
