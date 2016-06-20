598fee37e2fccc6966e15ce36e4b8eac045e4cad
jacobshiff <jacobshiff@gmail.com>
2016-06-20 15:30:18 -0400

    Tweaks to sessions controller, app controller, and conf

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,26 @@
-class SessionsController < ActionController::API
-  skip_before_action :authenticate, only: [:create]
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      skip_before_action :authenticate, only: [:create]
+      def create
+        binding.pry
+        customer = Customer.find_by(email: auth_params[:email])
+        if customer && customer.authenticate(auth_params[:password])
+          jwt = Auth.issue({customer: customer.id})
+          render json: {jwt: jwt}
+        else
+          render json: {:errors=>
+            [{:detail=>"incorrect email or password",
+              :source=>{:pointer=>"customer/err_type"}
+            }]
+          }, status: 404
+        end
+      end
 
-  def create
-    customer = Customer.find_by(email: auth_params[:email])
-    if customer && customer.authenticate(auth_params[:password])
-      jwt = Auth.issue({customer: customer.id})
-      render json: {jwt: jwt}
-    else
-      render json: {:errors=>
-        [{:detail=>"incorrect email or password",
-          :source=>{:pointer=>"customer/err_type"}
-        }]
-      }, status: 404
+      private
+        def auth_params
+          params.require(:auth).permit(:email, :password)
+        end
     end
   end
-
-  private
-    def auth_params
-      params.require(:auth).permit(:email, :password)
-    end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,17 @@ Bundler.require(*Rails.groups)
 
 module Rush
   class Application < Rails::Application
-    config.autoload_paths << Rails.root.join('lib') 
     config.api_only = true
+    config.autoload_paths << Rails.root.join('lib') 
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
